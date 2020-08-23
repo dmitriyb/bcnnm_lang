06622bfa5083708c7bd2b198738fda929d34e697
vlad0922@yandex.ru <vlad0922@yandex.ru>
2020-08-24 00:18:13 +0300

    [add] readme [fix] runner args

--- DSL_compiler/src/main/java/com/jetbrains/bcnnm/CompilerRunner.java
+++ DSL_compiler/src/main/java/com/jetbrains/bcnnm/CompilerRunner.java
@@ -1,30 +1,20 @@
 package com.jetbrains.bcnnm;
 
-import com.jetbrains.bcnnm.ArgumentParser;
 import com.jetbrains.bcnnm.translator.ProjectHandler;
-import net.objecthunter.exp4j.Expression;
-import net.objecthunter.exp4j.ExpressionBuilder;
-import net.objecthunter.exp4j.function.Function;
-import net.objecthunter.exp4j.operator.Operator;
-import net.objecthunter.exp4j.shuntingyard.ShuntingYard;
-import net.objecthunter.exp4j.tokenizer.*;
 
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-
+import java.util.Map;
 
 public class CompilerRunner {
     public static void main(String[] args) {
         ArgumentParser parser = new ArgumentParser();
         parser.addArgument("config");
+        parser.addArgument("out_path");
 
         Map<String, String> argumentValues = parser.parseArguments(args);
 
         ProjectHandler handler = new ProjectHandler(argumentValues.get("config"));
         handler.prefetchData();
-        handler.compile("test_out");
+        handler.compile(argumentValues.get("out_path"));
 
     }
 }
