1e3ad5cf747a8d3299ba5cc297906e0b7cea7afd
Georgy Galumov <georgyg2010@gmail.com>
2020-09-22 13:54:18 +0300

    Unknown filenames for constants and molecules; Useless ArgumentParser

--- DSL_translator/src/main/java/com/synstorm/translator/CompilerRunner.java
+++ DSL_translator/src/main/java/com/synstorm/translator/CompilerRunner.java
@@ -6,15 +6,15 @@ import java.util.Map;
 
 public class CompilerRunner {
     public static void main(String[] args) {
-        ArgumentParser parser = new ArgumentParser();
-        parser.addArgument("config");
-        parser.addArgument("out_path");
+//        ArgumentParser parser = new ArgumentParser();
+//        parser.addArgument("config");
+//        parser.addArgument("out_path");
+//
+//        Map<String, String> argumentValues = parser.parseArguments(args);
 
-        Map<String, String> argumentValues = parser.parseArguments(args);
-
-        ProjectHandler handler = new ProjectHandler(argumentValues.get("config"));
+        ProjectHandler handler = new ProjectHandler(args[0]);
         handler.prefetchData();
-        handler.compile(argumentValues.get("out_path"));
+        handler.compile(args[1]);
 
     }
 }
